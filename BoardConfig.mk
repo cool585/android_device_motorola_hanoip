0357aa5b3ef784708dca429eed16744670490f41
Anoosragh <ok.ano0s6090@gmail.com>
2023-12-25 13:34:56 +0000

    hanoip: Build kernel inline

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,14 +28,6 @@ DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 # Kernel
 TARGET_KERNEL_CONFIG := vendor/hanoip_defconfig
 
-# Kernel modules - Audio
-TARGET_MODULE_ALIASES += \
-    snd-soc-aw882xx.ko:snd_smartpa_aw882xx.ko \
-    snd-soc-cs35l41.ko:cirrus_cs35l41.ko \
-    snd-soc-wm-adsp.ko:cirrus_wm_adsp.ko \
-    wcd_spi_dlkm.ko:audio_wcd_spi.ko \
-    wcd934x_dlkm.ko:audio_wcd934x.ko
-
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
